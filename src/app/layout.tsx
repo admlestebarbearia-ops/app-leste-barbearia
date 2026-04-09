26aeab6b4a67df4d29954df1edaa76dd82ff8c25
Nilson <nilson.brites@gmail.com>
2026-04-09 07:57:23 -0300

    feat: icone do app alterado para logo2.png

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,7 +53,7 @@ export const metadata: Metadata = {
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
     ],
     apple: [
-      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+      { url: "/logo2.png", type: "image/png" },
     ],
     // Android/Chrome: ícones fornecidos via site.webmanifest
   },
